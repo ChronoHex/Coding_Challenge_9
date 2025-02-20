6f8b3d4177869c4e1e9f30ecb7a0c7c225a813dc
Eugene Rodriguez <er11@usf.edu>
2025-02-20 13:56:51 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -7,7 +7,7 @@ class Employee {
         this.salary = salary; // employee salary
     }; // employee constructor
     getDetails() {
-        return `Name: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: ${this.salary}}`;
+        return `Employee: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: ${this.salary}}`;
     }; // getting employee details
     calculateAnnualSalary() {
     return this.salary * 12; // calculate annual salary
@@ -16,3 +16,21 @@ class Employee {
 
 const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000); // new employee
 console.log(emp1.getDetails()); // print employee details
+
+// Task 2: Creating a Manager Class (Inheritance & Method Overriding)
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary); // call employee constructor
+        this.teamSize = teamSize; // Manager's team size
+    }; // manager constructor
+    getDetails() {
+        return `Manager: ${this.name}, Id: ${this.id}, Department: ${this.department}, Salary: ${this.salary}, Team Size ${this.teamSize}`;
+    } // manager details
+    calculateBonus() {
+        return this.salary * 12 * 0.10; // calculate bonus
+    }
+}; // manager class
+
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5); // new manager
+console.log(mgr1.getDetails()); // print manager details
+console.log(mgr1.calculateBonus()); // print manager bonus
